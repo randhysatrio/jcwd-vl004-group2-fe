162de18ecdbfe96aab84fd9ae910f469782a65f4
primawk <pwkuntoro@gmail.com>
2022-04-12 22:55:15 +0700

    dashboard ui design

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,16 +1,107 @@
-import { FaSearch } from "react-icons/fa";
+import {
+  FaSearch,
+  FaBell,
+  FaUserAlt,
+  FaHome,
+  FaBars,
+  FaShoppingBag,
+} from "react-icons/fa";
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="h-16 bg-white shadow-sm px-8 fixed w-full top-0 left-0 flex items-center">
+      <div className="h-16 bg-white shadow-sm pl-80 pr-8 fixed w-full top-0 left-0 flex items-center">
         <div className="relative">
-          <FaSearch className="absolute left-2 top-2 w-6 text-gray-400" />
+          <FaSearch className="absolute left-2 top-3 w-6 text-gray-400" />
           <input
             type="text"
             className="block w-72 shadow border-none rounded-3x1 focus:outline-none py-2 bg-gray-100 text-base text-gray-600 pl-11 pr-2"
           />
         </div>
+
+        <div className="ml-auto flex items-center">
+          <div>
+            <FaBell className="w-6 cursor-pointer hover:text-primary" />
+          </div>
+
+          <div className="ml-4 relative">
+            <div className="cursor-pointer">
+              <div>
+                <FaUserAlt
+                  id="dropdown"
+                  className="h-4 w-4 object-cover hover:text-primary"
+                />
+              </div>
+
+              <div
+                id="dropdown_content"
+                className="hidden absolute z-50 mt-2 rounded-md shadow-lg w-48 right-0 py-1 bg-white"
+              >
+                <div className="px-4 py-2 text-xs text-gray-400">
+                  Manage Account
+                </div>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 transition"
+                >
+                  Profile
+                </a>
+                <div className="border-t border-gray-100"></div>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 transition"
+                >
+                  Logout
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed left-0 top-0 w-72 h-full bg-gray-800 shadow-md z-10">
+        <div className="text-white font-bold text-base p-5 bg-gray-900">
+          Heisen Berg Co.
+        </div>
+        <div className="py-5">
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaHome className="w-5 mr-3" />
+            Dashboard
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaShoppingBag className="w-5 mr-3" />
+            Category
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaHome className="w-5 mr-3" />
+            Product
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaUserAlt className="w-5 mr-3" />
+            User
+          </a>
+        </div>
+      </div>
+
+      <div className="pt-24 pr-8 pl-80">
+        <h1 className="text-3xl text-gray-700 font-bold">
+          Welcome to Dashboard
+        </h1>
       </div>
     </div>
   );
