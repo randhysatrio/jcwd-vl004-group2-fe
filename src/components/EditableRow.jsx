6662c5a23416a229a7a3020b78cc915656ad0ea8
primawk <pwkuntoro@gmail.com>
2022-04-16 22:32:14 +0700

    add and edit product updat Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.

--- src/components/EditableRow.jsx
+++ src/components/EditableRow.jsx
@@ -1,3 +1,5 @@
+import "../assets/styles/editableRow.css";
+
 const EditableRow = ({
   editFormData,
   handleEditFormChange,
@@ -5,104 +7,112 @@ const EditableRow = ({
 }) => {
   return (
     <tr>
-      <td className="text-sm text-gray-700">{editFormData.id}</td>
-      <td>
-        <input
+      <td className="text-sm font-semibold text-left">{editFormData.id}</td>
+      <td className="text-sm font-semibold text-left">
+        <textarea
+          type="text"
+          name="unit"
+          value={editFormData.image}
+          onChange={handleEditFormChange}
+          required
+        />
+      </td>
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="text"
           required
           name="name"
-          defaultValue={editFormData.name}
+          value={editFormData.name}
           onChange={handleEditFormChange}
-          className="text-sm text-gray-700"
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="number"
           name="price_buy"
-          defaultValue={editFormData.price_buy}
+          value={editFormData.price_buy}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="number"
           name="price_sell"
-          defaultValue={editFormData.price_sell}
+          value={editFormData.price_sell}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="number"
           name="stock"
-          defaultValue={editFormData.stock}
+          value={editFormData.stock}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="text"
           name="unit"
-          defaultValue={editFormData.unit}
+          value={editFormData.unit}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="number"
           name="volume"
-          defaultValue={editFormData.volume}
+          value={editFormData.volume}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">{editFormData.stock_in_unit}</td>
-      <td className="text-sm text-gray-700">{editFormData.stock_in_unit}</td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        {editFormData.stock_in_unit}
+      </td>
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="text"
           name="description"
-          defaultValue={editFormData.description}
+          value={editFormData.description}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
-          type="text"
-          name="unit"
-          defaultValue={editFormData.image}
-          onChange={handleEditFormChange}
-          required
-        />
-      </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="text"
           name="appearance"
-          defaultValue={editFormData.appearance}
+          value={editFormData.appearance}
           onChange={handleEditFormChange}
           required
         />
       </td>
-      <td className="text-sm text-gray-700">
-        <input
+      <td className="text-sm font-semibold text-left">
+        <textarea
           type="number"
           name="categoryId"
-          defaultValue={editFormData.categoryId}
+          value={editFormData.categoryId}
           onChange={handleEditFormChange}
           required
         />
       </td>
-
-      <td>
-        <button type="submit">Save</button>
-        <button type="button" onClick={handleCancelClick}>
+      <td className="flex text-center py-8">
+        <button
+          className="py-2.5 px-6 text-white bg-primary hover:bg-blue-400 transition rounded-xl items-center"
+          type="submit"
+        >
+          Save
+        </button>
+        <button
+          className="py-2.5 px-6 text-white bg-red-500 hover:bg-red-400 transition rounded-xl items-center"
+          type="button"
+          onClick={handleCancelClick}
+        >
           Cancel
         </button>
       </td>
