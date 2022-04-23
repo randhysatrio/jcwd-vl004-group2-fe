0a71d40f583b60cb23ad14298771baa0e8cfdd77
primawk <pwkuntoro@gmail.com>
2022-04-23 19:33:03 +0700

    banner typo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,10 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-import '../assets/styles/banner.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "../assets/styles/Banner.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 const Banner = () => {
   const navigate = useNavigate();
@@ -28,11 +28,14 @@ const Banner = () => {
                 <span>Best materials for the best medicines</span>
               </div>
               <div className="w-96 text-lg font-semibold text-gray-50 mb-7">
-                <span>We offer only the best products for the best price. What are you waiting for?</span>
+                <span>
+                  We offer only the best products for the best price. What are
+                  you waiting for?
+                </span>
               </div>
               <div className="w-96 h-14 px-10">
                 <button
-                  onClick={() => navigate('/products')}
+                  onClick={() => navigate("/products")}
                   className="h-full w-full border-2 rounded-lg border-white font-bold text-2xl text-white hover:bg-white hover:text-sky-500 transition duration-200 active:scale-95"
                 >
                   Shop Now
@@ -49,7 +52,10 @@ const Banner = () => {
                 <span className="text-sky-700">Reinvented.</span>
               </div>
               <div className="w-[400px] text-center text-gray-50 text-lg font-semibold mb-8">
-                <span>We're revolutionizing the pharmaceutical industry and we would like you to be a part of it</span>
+                <span>
+                  We're revolutionizing the pharmaceutical industry and we would
+                  like you to be a part of it
+                </span>
               </div>
               <div className="w-96 h-14 px-10">
                 <button className="h-full w-full border-2 rounded-lg border-white font-bold text-2xl text-white hover:bg-white hover:text-sky-500 transition duration-200 active:scale-95">
@@ -68,9 +74,12 @@ const Banner = () => {
               </div>
               <div className="w-2/3 flex justify-center text-justify text-lg font-semibold text-white">
                 <span>
-                  Ever since the first vaccine was developed in 1796 to treat smallpox, several different methods have been created to
-                  develop successful vaccines. Today, those methods, known as vaccine technologies, are more advanced and use the latest
-                  technology to help protect the world from preventable diseases. Do you know all of them?
+                  Ever since the first vaccine was developed in 1796 to treat
+                  smallpox, several different methods have been created to
+                  develop successful vaccines. Today, those methods, known as
+                  vaccine technologies, are more advanced and use the latest
+                  technology to help protect the world from preventable
+                  diseases. Do you know all of them?
                 </span>
               </div>
               <div className="w-96 h-14 px-10">
