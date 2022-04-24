64e48b0532239f8ab10e4255573c8fde112cd697
primawk <pwkuntoro@gmail.com>
2022-04-24 23:59:29 +0700

    update multer in edit product

--- src/pages/EditProduct.jsx
+++ src/pages/EditProduct.jsx
@@ -5,11 +5,12 @@ import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../assets/constants';
 
 const EditProduct = () => {
-  const Swal = require('sweetalert2');
-  // useState([]) is different from useState({})
-  const [products, setProducts] = useState([]);
+  const Swal = require("sweetalert2");
+  // useState([]) is different from useState({}) findAll is array findbyPK is an object
+  const [products, setProducts] = useState({});
   const [categories, setCategories] = useState([]);
   const [images, setImage] = useState(null);
+  const [category, setCategory] = useState();
 
   const id = window.location.search.substring(1);
 
@@ -17,10 +18,11 @@ const EditProduct = () => {
     const res = await axios.get(`${API_URL}/product/find/${id}`);
     setImage(res.data.image);
     setProducts(res.data);
+    setCategory(res.data.categoryId);
   };
 
-  console.log(products);
-  console.log(images);
+  console.log(products.categoryId);
+  console.log(category);
 
   const fetchCategories = async () => {
     const res = await axios.get(`${API_URL}/category/all`);
@@ -28,8 +30,6 @@ const EditProduct = () => {
   };
 
   const fetchImagePreview = async () => {
-    // const res = await axios.get(`${API_URL}/product/find/${id}`);
-    // setImage(res.data.image);
     console.log(images);
     let preview = document.getElementById('imgpreview');
     preview.src = `${API_URL}/${images}`;
@@ -65,14 +65,18 @@ const EditProduct = () => {
       volume: volume.current.value,
       description: description.current.value,
       appearance: appearance.current.value,
-      categoryId: categoryId.current.value,
+      categoryId: parseInt(categoryId.current.value),
     };
-    formData.append('productData', JSON.stringify(newProduct));
-    formData.append('image', images);
+    formData.append("productData", JSON.stringify(newProduct));
+    if (images) {
+      formData.append("image", images);
+    }
+
     console.log(newProduct);
+
     try {
       await axios.patch(`${API_URL}/product/edit/${id}`, formData);
-      navigate('/dashboard');
+      navigate("/dashboard/product");
       Swal.fire({
         icon: 'success',
         // title: "Oops...",
@@ -171,17 +175,203 @@ const EditProduct = () => {
             ))}
           </select>
         </div>
-        <div className="flex">
-          <button
-            className="mt-8 py-2.5 px-6 text-white bg-primary hover:bg-blue-400 transition rounded-xl items-center mr-3"
-            onClick={handleEditFormSubmit}
+      </div>
+
+      <div className="fixed left-0 top-0 w-40 h-full bg-gray-800 shadow-md z-10">
+        <div className="text-white font-bold text-base p-5 bg-gray-900">
+          Heisen Berg Co.
+        </div>
+        <div className="py-5">
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
           >
-            Edit Product
-          </button>
-          <button className="mt-8 py-2.5 px-6 text-white bg-red-500 hover:bg-red-400 transition rounded-xl items-center">
-            <a href="http://localhost:3000/dashboard/product">Cancel</a>
-            {/*<Link> React-Router-dom APIURL + dashboard */}
-          </button>
+            <FaHome className="w-5 mr-3" />
+            Dashboard
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaShoppingBag className="w-5 mr-3" />
+            Category
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaHome className="w-5 mr-3" />
+            Product
+          </a>
+
+          <a
+            href="#"
+            className="flex items-center my-1 px-4 py-3 text-white border-1-4 border-transparent hover:bg-primary transition"
+          >
+            <FaUserAlt className="w-5 mr-3" />
+            User
+          </a>
+        </div>
+      </div>
+
+      <div className="pt-24 pr-8 pl-48">
+        <h1 className="text-3xl text-gray-700 font-bold mb-3">Edit Product</h1>
+        <div>
+          {/* <form> */}
+          <div className="grid grid-cols-6 gap-4 justify-items-star">
+            <div>
+              <label className="mr-3">Name:</label>
+            </div>
+            <div className="col-start-2 col-span-3">
+              <textarea
+                type="text"
+                name="name"
+                className="h-32 w-5/6"
+                required
+                defaultValue={products.name}
+                ref={name}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Description:</label>
+            </div>
+            <div className="col-start-2 col-span-3">
+              <textarea
+                type="text"
+                name="description"
+                className="h-32 w-5/6"
+                required
+                defaultValue={products.description}
+                ref={description}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Image:</label>
+            </div>
+            <div className="col-start-2 col-span-3">
+              <input
+                type="file"
+                name="image"
+                required
+                onChange={onBtAddFile}
+                accept="image/*"
+              />
+            </div>
+            <div className="col-start-2">
+              {/* profileImage ? URL.createObjectURL(profileImage) : ${API_URL}/${userData.profile_picture} */}
+              <img id="imgpreview" src={`${API_URL}/${products.image}`} />
+            </div>
+            <div className="col-start-1">
+              <label className="mr-3">Price Buy:</label>
+            </div>
+            <div className="col-start-2 col-span-3">
+              <input
+                type="number"
+                name="price_buy"
+                className=""
+                required
+                defaultValue={products.price_buy}
+                ref={price_buy}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Price Sell:</label>
+            </div>
+            <div className="col-start-2 col-span-3">
+              <input
+                type="number"
+                name="price_sell"
+                className=""
+                required
+                defaultValue={products.price_sell}
+                ref={price_sell}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Unit:</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="unit"
+                className=""
+                required
+                defaultValue={products.unit}
+                ref={unit}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Volume:</label>
+            </div>
+            <div>
+              <input
+                type="number"
+                name="volume"
+                className=""
+                required
+                defaultValue={products.volume}
+                ref={volume}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Stock:</label>
+            </div>
+            <div>
+              <input
+                type="number"
+                name="stock"
+                className=""
+                required
+                defaultValue={products.stock}
+                ref={stock}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Appearance:</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="appearance"
+                className=""
+                required
+                defaultValue={products.appearance}
+                ref={appearance}
+              />
+            </div>
+            <div className="col-start-1">
+              <label className="">Category:</label>
+            </div>
+            {/* <DropdownCategories /> */}
+            <select
+              name="categoryId"
+              ref={categoryId}
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              {/* <option value="">Choose a category</option> */}
+              {categories.map((item, index) => (
+                <option value={item.id} key={item?.id}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex">
+            <button
+              className="mt-8 py-2.5 px-6 text-white bg-primary hover:bg-blue-400 transition rounded-xl items-center mr-3"
+              onClick={handleEditFormSubmit}
+            >
+              Edit Product
+            </button>
+            <button className="mt-8 py-2.5 px-6 text-white bg-red-500 hover:bg-red-400 transition rounded-xl items-center">
+              <a href="http://localhost:3000/dashboard/product">Cancel</a>
+              {/*<Link> React-Router-dom APIURL + dashboard */}
+            </button>
+          </div>
+          {/* </form> */}
         </div>
         {/* </form> */}
       </div>
